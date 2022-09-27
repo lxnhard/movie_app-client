6218115d959f9954addfee9ceffbe62a48724f87
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-09-27 17:33:52 +0200

    style validation errors

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -4,6 +4,8 @@ import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+import './registration-view.scss';
+
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
