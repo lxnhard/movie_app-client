4f9a9c6bc2110d19b9a27212438c56629667abe8
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-09-29 17:42:02 +0200

    implement links to director/genre view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from "react-router-dom";
+
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
     return (
       <Card>
-        <Card.Img variant="top" src={movie.ImagePath} onClick={() => onMovieClick(movie)} alt={`Poster: ${movie.Title}`} title={movie.Title} className="image-link" />
+        <Link to={`/movies/${movie._id}`}>
+          <Card.Img variant="top" src={movie.ImagePath} alt={`Poster: ${movie.Title}`} title={movie.Title} className="image-link" />
+        </Link>
         <Card.Body>
           <Card.Title><h1 className="card-title">{movie.Title}</h1></Card.Title>
           <Card.Text>
@@ -37,6 +41,5 @@ MovieCard.propTypes = {
       Name: PropTypes.string.isRequired,
       Description: PropTypes.string
     }),
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  }).isRequired
 };
