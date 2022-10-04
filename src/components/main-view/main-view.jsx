9595bb066a0685227d525c2d3b9fba77167e8f14
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-10-04 17:19:31 +0200

    set movies as props for ProfileView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -158,7 +158,7 @@ export class MainView extends React.Component {
             <Route path={`/users/${user}`} render={({ history }) => {
               if (!user) { return <Redirect to="/" /> }
               return <Col md={8}>
-                <ProfileView user={user} onBackClick={() => history.goBack()} />
+                <ProfileView user={user} movies={movies} onBackClick={() => history.goBack()} />
               </Col>
             }
             } />
