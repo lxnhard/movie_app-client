ab072ff28cd667e92b8f20986592ec2a1d38ee4e
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-10-20 17:20:05 +0200

    add updateUser action + reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, UPDATE_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
@@ -22,6 +22,8 @@ function movies(state = [], action) {
 function user(state = '', action) {
   switch (action.type) {
     case SET_USER:
+      return action.value;
+    case UPDATE_USER:
       return action.value;
     default:
       return state;
