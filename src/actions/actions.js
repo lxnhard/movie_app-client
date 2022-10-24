f5d2795c25c36af077fb266f28278fa6e41d885a
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-10-24 18:45:38 +0200

    add user and favorite actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,6 +2,9 @@ export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
 export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const SET_FAVORITE = 'SET_FAVORITE';
+export const DELETE_FAVORITE = 'DELETE_FAVORITE';
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -17,4 +20,16 @@ export function setUser(value) {
 
 export function updateUser(value) {
   return { type: SET_USER, value };
+}
+
+export function deleteUser(value) {
+  return { type: DELETE_USER, value };
+}
+
+export function setFavorite(value) {
+  return { type: SET_FAVORITE, value };
+}
+
+export function deleteFavorite(value) {
+  return { type: DELETE_FAVORITE, value };
 }
